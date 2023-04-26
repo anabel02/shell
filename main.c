73dd557b71e1d4674e643377b0b5247520bf7778
anabel02 <anabelbenitezglez@gmail.com>
2023-04-26 17:32:15 -0400

    execute again syntax error

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 #include "utils.h"
 #include "execute.h"
 
-/* malloc comando mas largo por cantidad de again
- * help again*/
+/* malloc comando mas largo por cantidad de again*/
 
 char *replace_again(char *line);
 void lsh_loop();
@@ -94,7 +93,7 @@ char *replace_again(char *line) {
     for (j = again_pos + 5; line[j] != 0; ++j) {
         if (!is_special_char_or_blank(line[j])) {
             arg[arg_pos++] = line[j];
-        } else if (arg_pos > 0) {
+        } else if (arg_pos > 0 || is_special_char(line[j])) {
             break;
         }
     }
@@ -102,37 +101,29 @@ char *replace_again(char *line) {
     char *ptr;
     long history_command = strtol(arg, &ptr, 10);
 
-    if (ptr[0] != 0) {
-        printf("%s\n", "lsh: syntax error near again");
-        free(arg);
-        free(no_again_line);
-        return line;
+    if (ptr[0] != 0 || arg[0] == 0) {
     } else if (history_length == 0) {
         printf("%s\n", "lsh: history is empty");
-        free(arg);
-        free(no_again_line);
-        return line;
     } else if (history_command <= 0 || history_command > history_length) {
         printf("%s %ld\n", "lsh: history index out of range:", history_command);
-        free(arg);
-        free(no_again_line);
-        return line;
     }
 
-    strcpy(arg, history[history_command - 1]);
-    arg[strlen(history[history_command - 1]) - 1] = 0;
+    if (ptr[0] == 0 && history_command > 0 && history_command <= history_length) {
+        strcpy(arg, history[history_command - 1]);
+        arg[strlen(history[history_command - 1]) - 1] = 0;
+    } else if (history_length == 0) {
+        printf("%s\n", "lsh: history is empty");
+        again_pos += 5;
+    } else {
+        printf("%s %ld\n", "lsh: history index out of range:", history_command);
+        again_pos += 5;
+    }
 
     for (int i = 0; i < again_pos; ++i) {
         no_again_line[pos++] = line[i];
     }
     no_again_line[pos] = 0;
     strcat(no_again_line, arg);
-    char *post_again = replace_again(line + j);
-    if(post_again == NULL) {
-        free(arg);
-        free(no_again_line);
-        return line;
-    }
     free(arg);
-    return strcat(no_again_line, post_again);
+    return strcat(no_again_line, replace_again(line + j));
 }
